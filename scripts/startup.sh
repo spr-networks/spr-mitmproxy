1fd19aa4fec4f8f9586fb5ab68cf693553d54a8d
ltsrad <alex+rad@longterm.io>
2023-11-08 22:40:03 -0800

    Add compose file, startup script

---
+++ scripts/startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+nft -f - << EOF
+table inet nat {
+        chain prerouting {
+                type nat hook prerouting priority filter; policy accept;
+                meta nftrace set 1
+                tcp dport { 80, 443 } dnat ip to 127.0.0.1:9999
+        }
+}
+EOF
+
+mitmweb -p 9999 -m transparent --web-host 0.0.0.0
